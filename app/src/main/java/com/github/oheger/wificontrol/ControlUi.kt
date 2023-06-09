d102996e5e4dbc23d028cf37ad9299b4d9b415ee
Oliver Heger <oliver.heger@oliver-heger.de>
2023-06-09 21:30:05 +0200

    UI: Implemented the UI for different lookup states.
    Depending on the lookup state, the UI displays different texts and
    icons.
    
    The implementation is still incomplete; the final ServerFound state is
    not yet handled.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- app/src/main/java/com/github/oheger/wificontrol/ControlUi.kt
+++ app/src/main/java/com/github/oheger/wificontrol/ControlUi.kt
@@ -18,20 +18,166 @@
  */
 package com.github.oheger.wificontrol
 
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.oheger.wificontrol.ui.theme.WifiControlTheme
+
+internal const val TAG_SEARCHING_FOR_SERVER = "tag_searching_for_server"
+internal const val TAG_SEARCHING_HINT = "tag_searching_for_server_hint"
+internal const val TAG_SEARCHING_INDICATOR = "tag_searching_indicator"
+internal const val TAG_SERVER_NOT_FOUND = "tag_server_not_found"
+internal const val TAG_SERVER_NOT_FOUND_HINT = "tag_server_not_found_hint"
+internal const val TAG_WIFI_AVAILABLE = "tag_wifi_available"
+internal const val TAG_WIFI_UNAVAILABLE = "tag_wifi_unavailable"
+internal const val TAG_WIFI_UNAVAILABLE_HINT = "tag_wifi_unavailable_hint"
+internal const val TAG_WIFI_UNKNOWN = "tag_wifi_unknown"
+
+/**
+ * Generate the tag for a text line in the notification.
+ */
+internal fun textTag(tag: String): String = "${tag}_text"
+
+/**
+ * Generate the tag for an icon in the notification.
+ */
+internal fun iconTag(tag: String): String = "${tag}_icon"
 
 @Composable
 fun ControlUi(lookupState: ServerLookupState, modifier: Modifier = Modifier) {
-    when(lookupState) {
+    when (lookupState) {
         is ServerFound -> Text("Server found.")
-        else -> Text("Server not found.", modifier = modifier.testTag("foo"))
+
+        is NetworkStatusUnknown -> Notification(R.drawable.ic_unknown, TAG_WIFI_UNKNOWN, modifier) {
+            NotificationText(R.string.state_wifi_unknown, TAG_WIFI_UNKNOWN, modifier)
+        }
+
+        is WiFiUnavailable ->
+            Notification(R.drawable.ic_wifi_unavailable, TAG_WIFI_UNAVAILABLE, modifier) {
+                Column(modifier = modifier.fillMaxWidth()) {
+                    NotificationText(R.string.state_wifi_unavailable, TAG_WIFI_UNAVAILABLE, modifier)
+                    Text(
+                        stringResource(R.string.state_wifi_unavailable_hint),
+                        modifier = modifier.testTag(TAG_WIFI_UNAVAILABLE_HINT)
+                    )
+                }
+            }
+
+        is SearchingInWiFi ->
+            Column(modifier = modifier.fillMaxWidth()) {
+                WiFiAvailable(modifier)
+                Notification(R.drawable.ic_search_server, TAG_SEARCHING_FOR_SERVER, modifier) {
+                    Column(modifier = modifier.fillMaxWidth()) {
+                        NotificationText(R.string.state_searching, TAG_SEARCHING_FOR_SERVER, modifier)
+                        Text(
+                            stringResource(R.string.state_searching_hint),
+                            modifier = modifier.testTag(TAG_SEARCHING_HINT)
+                        )
+                        ProgressIndicator(modifier)
+                    }
+                }
+            }
+
+        is ServerNotFound ->
+            Column(modifier = modifier.fillMaxWidth()) {
+                WiFiAvailable(modifier)
+                Notification(R.drawable.ic_server, TAG_SERVER_NOT_FOUND, modifier) {
+                    Column(modifier = modifier.fillMaxWidth()) {
+                        NotificationText(R.string.state_server_not_found, TAG_SERVER_NOT_FOUND, modifier)
+                        Text(
+                            stringResource(R.string.state_server_not_found_hint),
+                            modifier = modifier.testTag(TAG_SERVER_NOT_FOUND_HINT)
+                        )
+                    }
+                }
+            }
     }
+}
+
+/**
+ * Generate a notification indicating the availability of the Wi-Fi.
+ */
+@Composable
+fun WiFiAvailable(modifier: Modifier) {
+    Notification(R.drawable.ic_wifi_available, TAG_WIFI_AVAILABLE, modifier) {
+        NotificationText(R.string.state_wifi_available, TAG_WIFI_AVAILABLE, modifier)
+    }
+}
+
+/**
+ * Generate a row with a notification about the progress of the lookup operation. A notification consists of an
+ * [icon][iconRes] and other [content] that is aligned right to the icon. The icon is assigned the given [tag].
+ */
+@Composable
+fun Notification(
+    iconRes: Int,
+    tag: String,
+    modifier: Modifier,
+    content: @Composable () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Icon(painter = painterResource(iconRes), contentDescription = null, modifier.testTag(iconTag(tag)))
+        Spacer(modifier.width(width = 4.dp))
+        content()
+    }
+}
+
+/**
+ * Generate the text of a notification from the given [resource ID][textRes]. Set the given [tag].
+ */
+@Composable
+fun NotificationText(textRes: Int, tag: String, modifier: Modifier) {
+    Text(
+        stringResource(textRes),
+        fontSize = 18.sp,
+        modifier = modifier.testTag(textTag(tag))
+    )
+}
+
+/**
+ * Generate an infinite progress indicator. This is used to show that currently a check for the server in the network
+ * is ongoing.
+ */
+@Composable
+fun ProgressIndicator(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val progressAnimationValue by infiniteTransition.animateFloat(
+        initialValue = 0.0f,
+        targetValue = 1.0f,
+        animationSpec = infiniteRepeatable(animation = tween(900))
+    )
+    CircularProgressIndicator(
+        progress = progressAnimationValue,
+        modifier = modifier
+            .size(16.dp)
+            .testTag(TAG_SEARCHING_INDICATOR)
+    )
 }
 
 @Preview
@@ -40,11 +186,20 @@ fun ControlUiPreview(
     @PreviewParameter(LookupStatePreviewProvider::class)
     lookupState: ServerLookupState
 ) {
-    ControlUi(lookupState)
+    WifiControlTheme {
+        ControlUi(lookupState)
+    }
 }
 
+/**
+ * A [PreviewParameterProvider] implementation to generate previews for all server lookup states.
+ */
 class LookupStatePreviewProvider : PreviewParameterProvider<ServerLookupState> {
     override val values: Sequence<ServerLookupState> = sequenceOf(
-        NetworkStatusUnknown, WiFiUnavailable, SearchingInWiFi, ServerFound("http://192.168.0.2:8088/")
+        NetworkStatusUnknown,
+        WiFiUnavailable,
+        ServerNotFound,
+        SearchingInWiFi,
+        ServerFound("http://www.example.org/")
     )
 }
--- app/src/test/java/com/github/oheger/wificontrol/ControlUiTest.kt
+++ app/src/test/java/com/github/oheger/wificontrol/ControlUiTest.kt
@@ -19,10 +19,13 @@
 package com.github.oheger.wificontrol
 
 import androidx.activity.compose.setContent
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import io.kotest.inspectors.forAll
+
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,11 +36,71 @@ class ControlUiTest {
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun test() {
-        composeTestRule.activity.setContent {
-            ControlUi(NetworkStatusUnknown)
-        }
+    fun `The NetworkStatusUnknown state is displayed correctly`() {
+        checkNotificationsForState(NetworkStatusUnknown, textTag(TAG_WIFI_UNKNOWN), iconTag(TAG_WIFI_UNKNOWN))
+    }
 
-        composeTestRule.onNodeWithTag("foo").assertExists()
+    @Test
+    fun `The WiFiUnavailable state is displayed correctly`() {
+        checkNotificationsForState(
+            WiFiUnavailable,
+            iconTag(TAG_WIFI_UNAVAILABLE),
+            textTag(TAG_WIFI_UNAVAILABLE),
+            TAG_WIFI_UNAVAILABLE_HINT
+        )
+    }
+
+    @Test
+    fun `The SearchingInWiFi state is displayed correctly`() {
+        checkNotificationsForState(
+            SearchingInWiFi,
+            iconTag(TAG_WIFI_AVAILABLE),
+            textTag(TAG_WIFI_AVAILABLE),
+            iconTag(TAG_SEARCHING_FOR_SERVER),
+            textTag(TAG_SEARCHING_FOR_SERVER),
+            TAG_SEARCHING_HINT,
+            TAG_SEARCHING_INDICATOR
+        )
+    }
+
+    @Test
+    fun `The ServerNotFound state is displayed correctly`() {
+        checkNotificationsForState(
+            ServerNotFound,
+            iconTag(TAG_WIFI_AVAILABLE),
+            textTag(TAG_WIFI_AVAILABLE),
+            iconTag(TAG_SERVER_NOT_FOUND),
+            textTag(TAG_SERVER_NOT_FOUND),
+            TAG_SERVER_NOT_FOUND_HINT
+        )
+    }
+
+    /**
+     * Check whether for the given [state] the elements with the given [expectedTags] (and only those) are visible.
+     */
+    private fun checkNotificationsForState(state: ServerLookupState, vararg expectedTags: String) {
+        val tagPrefixes = listOf(
+            TAG_SEARCHING_FOR_SERVER,
+            TAG_SERVER_NOT_FOUND,
+            TAG_WIFI_AVAILABLE,
+            TAG_WIFI_UNAVAILABLE,
+            TAG_WIFI_UNKNOWN
+        )
+        val allTags = listOf(
+            TAG_SEARCHING_HINT,
+            TAG_SEARCHING_INDICATOR,
+            TAG_WIFI_UNAVAILABLE_HINT,
+            TAG_SERVER_NOT_FOUND_HINT
+        ) + tagPrefixes.flatMap { prefix -> listOf(iconTag(prefix), textTag(prefix)) }
+        val expectedTagsSet = expectedTags.toSet()
+
+        composeTestRule.activity.setContent { ControlUi(state) }
+
+        allTags.filter { it in expectedTagsSet }.forAll { tag ->
+            composeTestRule.onNodeWithTag(tag).assertIsDisplayed()
+        }
+        allTags.filterNot { it in expectedTagsSet }.forAll { tag ->
+            composeTestRule.onNodeWithTag(tag).assertDoesNotExist()
+        }
     }
 }
