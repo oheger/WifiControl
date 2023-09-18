f353bd38c56bd75e1060c8e78af99ebec29dc300
Oliver Heger <oliver.heger@oliver-heger.de>
2023-09-18 12:22:38 +0200

    build: Fixed a deprecation warning.
    Renamed `packagingOptions` to `packaging` as indicated by an IDE
    warning.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.7"
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
