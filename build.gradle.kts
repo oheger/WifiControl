287875b9c7ffb3a840161b5a1a08551cc309e54d
Oliver Heger <oliver.heger@oliver-heger.de>
2024-01-14 19:04:38 +0100

    build: Integrated the Catalog version update plugin [1].
    This plugin should simplify updating the versions of dependencies.
    Note that it required a reformat of the module declarations in the
    `libs.version.toml` file.
    
    [1]: https://github.com/littlerobots/version-catalog-update-plugin
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,31 @@
  * SPDX-License-Identifier: Apache-2.0
  * License-Filename: LICENSE
  */
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+
+    alias(libs.plugins.versions)
+    alias(libs.plugins.catalog.update)
+}
+
+/*
+ * Code from [1] to check whether a version is considered as stable.
+ * [1] https://github.com/ben-manes/gradle-versions-plugin
+ */
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+// https://github.com/ben-manes/gradle-versions-plugin
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
 }
