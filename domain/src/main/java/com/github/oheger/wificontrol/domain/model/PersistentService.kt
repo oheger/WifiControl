7193d4adbe0c044850b0e29395b143d27b669545
Oliver Heger <oliver.heger@oliver-heger.de>
2024-02-10 21:27:56 +0100

    domain: Added PersistentService class.
    This data class is going to be used to persist the services controlled
    by this app. It contains the full service definition and optional
    properties controlling how to lookup the service in the network.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ domain/src/main/java/com/github/oheger/wificontrol/domain/model/PersistentService.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023-2024 Oliver Heger.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+package com.github.oheger.wificontrol.domain.model
+
+import kotlin.time.Duration
+
+/**
+ * A data class describing a service controlled by this app in a format that can be persisted.
+ *
+ * An instance contains a [ServiceDefinition], since all properties here are mandatory. It also contains nullable
+ * properties corresponding to a [LookupConfig]. Undefined properties in this category are replaced by default values.
+ */
+data class PersistentService(
+    /** The underlying [ServiceDefinition]. */
+    val serviceDefinition: ServiceDefinition,
+
+    /** The optional [LookupConfig.networkTimeout]. */
+    val networkTimeout: Duration?,
+
+    /** The optional [LookupConfig.retryDelay]. */
+    val retryDelay: Duration?,
+
+    /** The optional [LookupConfig.sendRequestInterval]. */
+    val sendRequestInterval: Duration?
+)
