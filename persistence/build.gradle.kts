e9574e750a0809d9f04ba39e64f612709341cd26
Oliver Heger <oliver.heger@oliver-heger.de>
2024-02-17 16:47:45 +0100

    build: Added a module for the persistence layer.
    This module is going to provide the implementation for data sources
    defined in the repository module.
    
    Signed-off-by: Oliver Heger <oliver.heger@oliver-heger.de>

---
+++ persistence/build.gradle.kts
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2023-2024 Oliver Heger.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+plugins {
+    kotlin("kapt")
+
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.hilt.dagger)
+    alias(libs.plugins.kotlin.android)
+}
+
+val defaultCompileSdk: String by project
+val defaultMinSdk: String by project
+val defaultJvmTarget: String by project
+val javaCompileVersion: String by project
+
+android {
+    namespace = "com.github.oheger.wificontrol.persistence"
+    compileSdk = defaultCompileSdk.toInt()
+
+    defaultConfig {
+        minSdk = defaultMinSdk.toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(javaCompileVersion)
+        targetCompatibility = JavaVersion.toVersion(javaCompileVersion)
+    }
+    kotlinOptions {
+        jvmTarget = defaultJvmTarget
+    }
+    testOptions {
+        unitTests.all {
+            it.useJUnitPlatform()
+        }
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
+}
+
+kotlin {
+    // This is needed to avoid an error due to incompatible Java target versions between the
+    // 'compileReleaseJavaWithJavac' and 'kaptGenerateStubsReleaseKotlin' tasks; the latter uses as target the
+    // local JDK used to execute the Gradle build.
+    jvmToolchain(8)
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":repository"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.hilt.android)
+
+    kapt(libs.hilt.compiler)
+
+    testImplementation(libs.kotest.assertions)
+    testImplementation(libs.kotest.runner.junit)
+    testImplementation(libs.mockk)
+}
